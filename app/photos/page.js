89b76488a59afea9b28505d0e30bbca256c13ad6
Sriram Kolli <sriramkolli@gozennith.com>
2025-09-14 19:02:07 +0000

    "Image Optimization"

--- app/photos/page.js
+++ app/photos/page.js
@@ -1,3 +1,11 @@
+import Image from 'next/image'
+
+import dog1 from './../../public/images/dog1.png'
+import dog2 from './../../public/images/dog2.png'
+import dog3 from './../../public/images/dog3.png'
+import dog4 from './../../public/images/dog4.png'
+
+
 export default function PhotosPage() {
   return (
     <div>
@@ -5,18 +13,18 @@ export default function PhotosPage() {
         My Photos
       </h1>
 
-      <div className="grid grid-cols-2 gap-4">
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog1.png" className="object-cover w-full h-full" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="h-60 overflow-hidden relative">
+          <Image fill src={dog1} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" priority={true} quality={50} placeholder="blur" />
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog2.png" className="object-cover w-full h-full" />
+        <div className="h-60 overflow-hidden relative">
+          <Image fill src={dog2} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" priority={true} quality={50} placeholder="blur"/>
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog3.png" className="object-cover w-full h-full" />
+        <div className="h-60 overflow-hidden relative">
+          <Image fill src={dog3} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw" quality={50} placeholder="blur"/>
         </div>
-        <div className="h-60 overflow-hidden">
-          <img src="/images/dog4.png" className="object-cover w-full h-full" />
+        <div className="h-60 overflow-hidden relative">
+          <Image fill src={dog4} className="object-cover w-full h-full" alt="Picture of my dog" sizes="(max-width: 768px) 100vw, 50vw"  quality={50} placeholder="blur"/>
         </div>
       </div>
     </div>
