536a4fd8b9e7a5140ceba72f7f05881781776dad
Sriram Kolli <sriramkolli@gozennith.com>
2025-09-15 10:14:47 +0000

    Section 10: Next.js 101: Fonts, Dynamic OpenGraph Images, Assets & Metadata (SEO)

--- app/photos/page.js
+++ app/photos/page.js
@@ -6,6 +6,10 @@ import dog3 from './../../public/images/dog3.png'
 import dog4 from './../../public/images/dog4.png'
 
 
+export const metadata = {
+  title: 'Photos'
+}
+
 export default function PhotosPage() {
   return (
     <div>
